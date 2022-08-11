ef52bfe2da592f7f688d063cf3261d5b0da54fe6
Alfred Githinji <alfredgithinji87@gmail.com>
2022-08-11 17:08:21 +0300

    form fully responsive

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const Button = ({ text, href, onClick }) => {
   return (
     <motion.button
-      className="text-lg px-4 py-2 rounded-md bg-rose-500 text-white hover:bg-rose-600"
+      className="text-lg px-4 py-2 rounded-md bg-green-600 text-white hover:bg-rose-600"
       whileInView={{ scale: [null, 1.2, 1.0] }}
       href={href}
       onClick={onClick}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -77,12 +77,12 @@ const Form = () => {
   };
   return (
     <>
-      <form className="border-2 border-orange-700 flex-col items-center justify-center mt-10 px-5 py-5">
+      <form className="m-2 flex-col items-center justify-center mt-10 px-5 py-5">
         <h1
           id="form__heading"
           className=" flex items-center uppercase justify-center font-extrabold text-2xl"
         >
-          Transaction Details
+          Transaction Mockup
         </h1>
 
         <div className="flex phone:flex-col justify-around items-center my-10">
@@ -114,11 +114,11 @@ const Form = () => {
           ></input>
         </div>
         <div className="flex phone:flex-col  items-center justify-center my-5">
-          <div className="w-2/3 flex phone:flex-col items-center justify-center ">
-            <label for="contact" className="w-1/5">
+          <div className="w-2/3 flex phone:w-full  phone:my-1  phone:flex-col items-center justify-center ">
+            <label for="contact" className="w-1/5 phone:w-full">
               Contact
             </label>
-            <div className="phone:flex  phone:items-center">
+            <div className=" phone:flex phone:w-full phone:items-center">
             <input
               className="px-4 w-1/5 phone:w-2/5 phone:m-0  bg-white-200 appearance-none py-2 mr-1 border-2 border-green-400 rounded text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               type="Text"
@@ -127,7 +127,7 @@ const Form = () => {
               readOnly
             />
             <input
-              className="w-3/5 phone:w-3/5 phone:m-0 phone:px-0  bg-white-200 appearance-none ml-2 mr-10  border-2 border-green-400 rounded  py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              className="w-3/4 phone:w-full phone:ml-2  bg-white-200 appearance-none ml-2  border-2 border-green-400 rounded  py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               id="contact"
               type="Number"
               placeholder="Safaricom No."
@@ -139,12 +139,12 @@ const Form = () => {
             />
             </div>
           </div>
-          <div className="w-1/3 phone:w-full flex items-center justify-center ">
-            <label for="contact" className="w-2/5">
+          <div className="w-1/3 phone:w-full phone:my-1  phone:flex flex items-center">
+            <label for="contact" className=" w-2/5">
               Amount
             </label>
             <input
-              className="w-3/5 bg-white-200 appearance-none border-2 border-green-400 rounded  py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              className="w-3/5 phone:w-3/5 phone:m-1 bg-white-200 appearance-none border-2 border-green-400 rounded  py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               id="amount"
               type="Number"
               placeholder=" Enter Amount"
@@ -159,9 +159,9 @@ const Form = () => {
 
         <div className="flex items-center justify-between my-5 mx-5"></div>
 
-        <div className="flex justify-around items-center w-100 my-5 ">
-          <Button type="button" text="Mpesa Express" onClick={mpesaExpress} />
-          <Button type="button" text="B2C" onClick={B2C} />
+        <div className="flex px-5 justify-around items-center border-2 border-red-700 my-3 w-full">
+          <Button type="button" text="Lipa na Mpesa" onClick={mpesaExpress} />
+          <Button type="button" text="Business to Customer" onClick={B2C} />
         </div>
 
         {responseTracker ? (
