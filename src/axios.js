233d46bb2a8ec5cb5a9c52667034f31ca2034548
Alfred Githinji <alfredgithinji87@gmail.com>
2022-10-22 14:39:46 +0300

    Modified backend link

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // # CHANGING THE REPORTING OFFICER
-  baseURL: "https://daraja-integration.herokuapp.com/",
+  baseURL: "https://daraja-simulation.herokuapp.com/",
   // baseURL: "http://localhost:3003/",
 });
 export default instance;
